76cf8354f14007a260b5e9d28ec1645a9e3301c8
wukai <wukai6038@163.com>
2022-07-14 00:15:06 +0800

    add BST code

---
+++ src/cn/wukai/algorithm/tree/BinarySearchTree.java
@@ -0,0 +1,24 @@
+package cn.wukai.algorithm.tree;
+
+/**
+ * 1.二叉搜索树
+ * 2.若左子树不空，则左子树上所有结点的值均小于或等于它的根结点的值
+ * 3.若右子树不空，则右子树上所有结点的值均大于或等于它的根结点的值
+ * 4.左、右子树也分别为二叉排序树
+ * @author wukai
+ *
+ */
+
+public class BinarySearchTree {
+
+	private Node node;
+	
+	/**
+	 * 插入节点,构建二叉搜索树（BST）
+	 * @param data
+	 */
+	public void insert(int data) {
+		
+	}
+	
+}
